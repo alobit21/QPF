e6d105167f4a1cc81c71ffb232bbacc2e01b3b96
alobit21 <macmtavangu@gmail.com>
2026-07-05 01:57:19 +0300

    feat: seeded database and implemented amenity and slot models

--- spacefinder_frontend/test/widget_test.dart
+++ spacefinder_frontend/test/widget_test.dart
@@ -11,20 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:spacefinder_frontend/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Basic smoke test', (WidgetTester tester) async {
+    // We removed the default Flutter counter app (MyApp). 
+    // This is a placeholder test to keep the test runner happy.
+    expect(true, isTrue);
   });
 }
